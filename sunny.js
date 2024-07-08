0d3ac3aa56312b74f0945d250870a4af5f0c4a2d
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-09 00:36:35 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,47 @@
+// // // functions with objects
+
+// // // fun k andr multiple values pass karna seekho by ...
+// // function calcarpri (...num1){
+// //     return num1
+// // }
+// // console.log(calcarpri(200,200,300,400));
+// // // val1 and 2 phli and dsre position ko occupy karlengy 
+
+// // function calcarprice (val1,val2,...num1){
+// //     return num1
+// // }
+// // console.log(calcarprice(200,200,300,400));
+// let user = {
+//     name1:"sanaiyla",
+//     price: 578
+// }
+// // user k bjae u can insert anyobject taa k generic rahay
+// function objecthandle(user){
+//     console.log(`name is ${user.name1} and price os her course is ${user.price}`);
+// }
+// objecthandle(user)
+
+
+// // incorporating array within function
+
+// let myarray = [1,2,3,4]
+// function getfirstvalue(myarray){
+// return myarray[2]
+// }
+// console.log(myarray[2]);
+let sunny = {
+    name1:"sanaiyla",
+    id:"87e",
+}
+function kato (sunny){
+    console.log(`hello my name is ${sunny.name1} and my id is ${sunny.id}`);
+}
+kato(sunny)
+
+
+
+let myarray = [300,3,3,3,]
+function ashee(myarray){
+console.log(myarray);
+}
+ashee(myarray[2])
